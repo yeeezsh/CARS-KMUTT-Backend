6ede4c45e2d0b5f6bcc0a8317487d0e7783690c0
yee2542 <yee2542gtl@gmail.com>
2019-10-18 01:11:08 +0700

    import date scalar

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { DatabaseModule } from '../database/database.module';
 import { usersProviders } from './users.providers';
+import { DateScalar } from '../common/date.scalar';
 
 @Module({
   imports: [DatabaseModule],
   providers: [
+    DateScalar,
     UsersService, ...usersProviders,
     UsersResolver,
   ],
