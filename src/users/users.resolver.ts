eafc55845bd48d1416cb3d69625c9b9c6b7d21fa
yee2542 <yee2542gtl@gmail.com>
2019-10-18 01:55:31 +0700

    preparing users query & mutation  via graphql

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+
+// inputs
+import { CreateStaffInput } from './dtos/create-staff.input';
+
+// dtos
+import { StaffDto } from './dtos/staff.dto';
 
 @Resolver('Users')
 export class UsersResolver {
 
-}
+    constructor(
+        private readonly usersService: UsersService,
+    ) { }
 
+    @Query(() => [StaffDto])
+    async staff() {
+        return await this.usersService.listStaff();
+    }
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+
+// interfaces
+import { Staff } from './interfaces/staff.interface';
+
+// dtos
+import { CreateStaffInput } from './dtos/create-staff.input';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @Inject('STAFF_MODEL') private readonly staffModel: Model<Staff>,
+    ) { }
+
+    async createStaff(create: CreateStaffInput): Promise<Staff> {
+        const doc = new this.staffModel(create);
+        const saved = await doc.save();
+        return saved;
+    }
+
+    async listStaff(): Promise<Staff[]> {
+        const doc = await this.staffModel.find({});
+        return doc;
+    }
+
+}
