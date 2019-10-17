f802d8a5d63305e4c4064d92d5d84aa0dd6b47b3
yee2542 <yee2542gtl@gmail.com>
2019-10-18 00:31:43 +0700

    fix bad import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { usersProviders } from './users.providers';
 
 @Module({
