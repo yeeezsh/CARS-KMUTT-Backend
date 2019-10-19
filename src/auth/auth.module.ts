1b9e9363f4e96d7db1abc6fe1fa6e0b5ff57593e
yee2542 <yee2542gtl@gmail.com>
2019-10-20 04:30:57 +0700

    implementing auth service with JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersService,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly usersService: UsersService,
+    ) { }
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.usersService.loginStaff({ username, password });
+        if (user) { return user; }
+        return null;
+    }
+
+    async login(user: any) {
+        const payload = { username: user.username, sub: user.userId };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+
+}
