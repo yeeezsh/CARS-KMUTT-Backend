e5cf48e1ca08c0c0376c3f0d4aedeba1f8f1a794
yee2542 <yee2542gtl@gmail.com>
2019-10-20 03:23:19 +0700

    refactor to promise base

--- src/users/helpers/hash.ts
+++ src/users/helpers/hash.ts
@@ -4,20 +4,24 @@ const saltRounds = 10;
 
 export class Hash {
 
-    encrypt(plaintext: string): Promise<string> {
-        return genSalt(saltRounds, (err, salt) => {
-            if (err) { throw err; }
-            hash(plaintext, salt, (errx, hashed: string) => {
-                if (err || errx) { throw err || errx; }
-                return hashed;
+    static encrypt(plaintext: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            genSalt(saltRounds, (err, salt) => {
+                if (err) { throw err; }
+                hash(plaintext, salt, (errx, hashed: string) => {
+                    if (err || errx) { reject(err || errx); }
+                    resolve(hashed);
+                });
             });
         });
     }
 
-    compare(plaintext: string, hashed: string): Promise<boolean> {
-        return compare(plaintext, hashed, (err, res) => {
-            if (err) { throw err; }
-            return res;
+    static compare(plaintext: string, hashed: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            compare(plaintext, hashed, (err, res) => {
+                if (err) { reject(err); }
+                resolve(res);
+            });
         });
     }
 
