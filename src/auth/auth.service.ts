94f0c0ba292f723ea970e36a7468bb2f78addae1
yee2542 <yee2542gtl@gmail.com>
2019-10-20 18:13:20 +0700

    add logn & valiadateStaff method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import { Staff } from '../users/interfaces/staff.interface';
 
 @Injectable()
 export class AuthService {
@@ -10,17 +11,32 @@ export class AuthService {
         private readonly usersService: UsersService,
     ) { }
 
-    async validateUser(username: string, password: string): Promise<any> {
-        const user = await this.usersService.loginStaff({ username, password });
-        if (user) { return user; }
+    async validateStaff(username: string, inputPassword: string): Promise<any> {
+        const user = await this.usersService.loginStaff({ username, password: inputPassword });
+        if (user) {
+            const { password, ...result } = user;
+            return result;
+        }
         return null;
     }
 
-    async login(user: any) {
-        const payload = { username: user.username, sub: user.userId };
-        return {
-            access_token: this.jwtService.sign(payload),
-        };
-    }
+    // async login(user: any) {
+    //     const payload = { username: user.username, sub: user.userId };
+    //     return {
+    //         access_token: this.jwtService.sign(payload),
+    //     };
+    // }
 
+    public async login(username: string, inputPassword: string): Promise<any | { status: number }> {
+        return this.validateStaff(username, inputPassword).then((user) => {
+            if (!user) {
+                return { status: 404 };
+            }
+            const payload = { username: user.username, sub: user.userId };
+            return {
+                access_token: this.jwtService.sign(payload),
+            };
+
+        });
+    }
 }
