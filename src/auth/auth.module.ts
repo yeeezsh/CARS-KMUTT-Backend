81c1dfe887d7b0dcc3d6c4f58e14a39c3faef55e
yee2542 <yee2542gtl@gmail.com>
2019-10-21 09:41:24 +0700

    register jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,8 +4,10 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { StaffJWTStrategy } from './staff.jwt.strategy';
-import { StaffStrategey } from './staff.strategy';
+import { StaffStrategy } from './staff.strategy';
 import { UsersModule } from '../users/users.module';
+import { RequestorStrategy } from './requestor.strategy';
+import { RequestorJWTStrategy } from './requestor.jwt.strategy';
 
 @Module({
   imports: [
@@ -17,7 +19,13 @@ import { UsersModule } from '../users/users.module';
     }),
 
   ],
-  providers: [AuthService, StaffStrategey, StaffJWTStrategy],
+  providers: [
+    AuthService,
+    StaffStrategy,
+    StaffJWTStrategy,
+    RequestorStrategy,
+    RequestorJWTStrategy,
+  ],
   exports: [AuthService],
 })
 export class AuthModule { }
