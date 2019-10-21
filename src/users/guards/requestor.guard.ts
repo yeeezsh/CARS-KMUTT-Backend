b0158e18b39579046b65ed7e14b2551a089a5fdb
yee2542 <yee2542gtl@gmail.com>
2019-10-21 10:01:40 +0700

    add guard graphql for requestor

---
+++ src/users/guards/requestor.guard.ts
@@ -0,0 +1,11 @@
+import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class GqlRequestorGuard extends AuthGuard('requestorJWT') {
+    getRequest(context: ExecutionContext) {
+        const ctx = GqlExecutionContext.create(context);
+        return ctx.getContext().req;
+    }
+}
