79ce7c2950de0915829aa072891e638dae3c125e
yee2542 <yee2542gtl@gmail.com>
2019-10-23 01:58:46 +0700

    refactor error handling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,38 +22,30 @@ export class UsersService {
     ) { }
 
     async createStaff(create: CreateStaffInput): Promise<Staff> {
-        try {
-            const duplicated = await Promise.all([
-                this.staffModel.findOne({ username: create.username }),
-                this.staffModel.findOne({ email: create.email }),
-            ]);
-            const valid = duplicated.filter(e => e);
-            if (valid.length !== 0) {
-                throw new HttpException('username or email is duplicated', HttpStatus.BAD_REQUEST);
-            }
-
-            const parse: CreateStaffInput = {
-                ...create,
-                password: await Hash.encrypt(create.password),
-            };
-            const doc = new this.staffModel(parse);
-            const saved = await doc.save();
-            return saved;
-        } catch (err) {
-            throw err;
+        const duplicated = await Promise.all([
+            this.staffModel.findOne({ username: create.username }),
+            this.staffModel.findOne({ email: create.email }),
+        ]);
+        const valid = duplicated.filter(e => e);
+        if (valid.length !== 0) {
+            throw new HttpException('username or email is duplicated', HttpStatus.BAD_REQUEST);
         }
+
+        const parse: CreateStaffInput = {
+            ...create,
+            password: await Hash.encrypt(create.password),
+        };
+        const doc = new this.staffModel(parse);
+        const saved = await doc.save();
+        return saved;
     }
 
     async loginStaff(login: StaffLoginDto): Promise<Staff> {
-        try {
-            const user: Staff = await this.staffModel.findOne({ username: login.username }).lean();
-            if (!user) { throw new HttpException('user is not exist', HttpStatus.UNAUTHORIZED); }
-            const auth = await Hash.compare(login.password, user.password);
-            if (!auth) { throw new HttpException('password invalid', HttpStatus.UNAUTHORIZED); }
-            return user;
-        } catch (err) {
-            throw err;
-        }
+        const user: Staff = await this.staffModel.findOne({ username: login.username }).lean();
+        if (!user) { throw new HttpException('user is not exist', HttpStatus.UNAUTHORIZED); }
+        const auth = await Hash.compare(login.password, user.password);
+        if (!auth) { throw new HttpException('password invalid', HttpStatus.UNAUTHORIZED); }
+        return user;
     }
 
     async loginRequestor(login: RequestorLoginDto): Promise<Requestor> {
