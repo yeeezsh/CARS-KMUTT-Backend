7b284521544bf4b95ea5f8b1feabc69f69d3f9f9
yee2542 <yee2542gtl@gmail.com>
2019-10-23 01:50:17 +0700

    add area & area.type dtio

---
+++ src/area/dtos/area.dto.ts
@@ -0,0 +1,35 @@
+
+import { IsNotEmpty, IsString, IsIn, IsDate, IsNumber } from 'class-validator';
+import { ObjectType, Field } from 'type-graphql';
+
+@ObjectType()
+export class AreaDto {
+    @Field()
+    @IsNotEmpty()
+    // tslint:disable-next-line: variable-name
+    readonly _id: string;
+
+    @Field()
+    @IsNotEmpty()
+    @IsString()
+    readonly type: string;
+
+    // @Field()
+    // @IsNotEmpty()
+    // @IsString()
+    // readonly form: string; // required form module
+
+    @Field()
+    @IsNumber()
+    readonly maxTask: number;
+
+    @Field()
+    @IsNotEmpty()
+    @IsDate()
+    readonly createAt: Date;
+
+    @Field()
+    @IsNotEmpty()
+    @IsDate()
+    readonly updateAt: Date;
+}
---
+++ src/area/dtos/area.input.ts
@@ -0,0 +1,25 @@
+
+import { IsNotEmpty, IsString, IsDate, IsNumber } from 'class-validator';
+import { Field, InputType } from 'type-graphql';
+
+@InputType()
+export class CreateAreaInput {
+    @Field(() => String)
+    @IsNotEmpty()
+    @IsString()
+    readonly type: string;
+
+    @Field(() => String)
+    @IsNotEmpty()
+    @IsString()
+    readonly form: string;
+
+    @Field(() => Number)
+    @IsNotEmpty()
+    @IsNumber()
+    readonly maxTask: number;
+
+    @Field(() => Date)
+    @IsDate()
+    readonly updateAt: Date;
+}
