af620890a822a16ef465faf058d492f87571c813
yee2542 <yee2542gtl@gmail.com>
2019-10-23 02:04:48 +0700

    add simple create & list area services

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, HttpStatus, HttpException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Area } from './interfaces/area.interface';
+import { AreaType } from './interfaces/area.type.interface';
+import { AreaTypeDto } from './dtos/area.type.dto';
+import { AreaDto } from './dtos/area.dto';
 
 @Injectable()
-export class AreaService {}
+export class AreaService {
+
+    constructor(
+        @Inject('AREA_MODEL') private readonly areaModel: Model<Area>,
+        @Inject('AREA_TYPE_MODEL') private readonly areaTypeModel: Model<AreaType>,
+    ) { }
+
+    async createAreaType(data: AreaTypeDto): Promise<AreaType> {
+        try {
+            const duplicated = this.areaTypeModel.findOne({ label: data.label })
+            if (duplicated) {
+                throw new HttpException('label duplicated', HttpStatus.UNAUTHORIZED);
+            }
+            const doc = new this.areaTypeModel(data);
+            const saved = await doc.save();
+            return saved;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async createArea(data: AreaDto): Promise<Area> {
+        try {
+            const duplicated = this.areaModel.findOne({ label: data.label });
+            if (duplicated) {
+                throw new HttpException('label duplicated', HttpStatus.UNAUTHORIZED);
+            }
+            const doc = new this.areaModel(data);
+            const saved = await doc.save();
+            return saved;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async listAreaType(): Promise<AreaType[]> {
+        try {
+            const doc = this.areaTypeModel.find({}).lean();
+            return doc;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async listArea(): Promise<Area[]> {
+        try {
+            const doc = this.areaModel.find({}).lean();
+            return doc;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+}
