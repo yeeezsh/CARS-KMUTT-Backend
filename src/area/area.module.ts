28468bc133701dbd4ac5d69ad0f21aa1f3144b4f
yee2542 <yee2542gtl@gmail.com>
2019-10-23 01:59:58 +0700

    register area providers

--- src/area/area.module.ts
+++ src/area/area.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AreaService } from './area.service';
 import { AreaResolver } from './area.resolver';
 import { DatabaseModule } from '../database/database.module';
+import { areaProviders } from './area.providers';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [AreaService, AreaResolver],
+  providers: [
+    AreaService, AreaResolver,
+    ...areaProviders,
+  ],
 })
 export class AreaModule { }
