e0f065e2efe4b3c6d5fe726ab64db537ba7ec35a
yee2542 <yee2542gtl@gmail.com>
2019-10-23 01:59:31 +0700

    Revert "register area providers"
    This reverts commit 6bd1060e7be50c47abc6cb71640593c091a14652.

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -1,30 +1,4 @@
-import { Injectable, Inject, HttpStatus, HttpException } from '@nestjs/common';
-import { Model } from 'mongoose';
-import { Area } from './interfaces/area.interface';
-import { AreaType } from './interfaces/area.type.interface';
-import { AreaTypeDto } from './dtos/area.type.dto';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AreaService {
-
-    constructor(
-        @Inject('AREA_MODEL') private readonly areaModel: Model<Area>,
-        @Inject('AREA_TYPE_MODEL') private readonly areaTypeModel: Model<AreaType>,
-    ) { }
-
-    async createAreaType(data: AreaTypeDto): Promise<AreaType> {
-        try {
-            const duplicated = this.areaTypeModel.findOne({ label: data.label })
-            if (duplicated) {
-                throw new HttpException('label duplicated', HttpStatus.UNAUTHORIZED);
-            }
-            const doc = new this.areaTypeModel(data);
-            const saved = await doc.save();
-            return saved;
-        } catch (err) {
-            throw err;
-        }
-    }
-
-
-}
+export class AreaService {}
