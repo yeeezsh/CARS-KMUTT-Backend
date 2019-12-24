925853295d3b2432fddf14a20447d2a9fcf0cf60
yee2542 <yee2542gtl@gmail.com>
2019-12-24 22:08:02 +0700

    integrating user guard

--- src/users/guards/staff.guard.ts
+++ src/users/guards/staff.guard.ts
@@ -1,11 +1,23 @@
 import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 @Injectable()
 export class GqlStaffGuard extends AuthGuard('staffJWT') {
+// export class GqlStaffGuard {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
   }
+
+  // handleRequest(err, user, info: Error) {
+  //   if (info instanceof TokenExpiredError) {
+  //     // do stuff when token is expired
+  //     console.log('token expired');
+  //   }
+  //   // return user;
+  //   return null;
+  // }
+
 }
