16d78f221cd14abfd32a323f522f75187148d067
yee2542 <yee2542gtl@gmail.com>
2019-12-25 00:36:25 +0700

    preparing basic area op

--- src/area/area.resolver.ts
+++ src/area/area.resolver.ts
@@ -1,4 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+import { AreaService } from './area.service';
 
 @Resolver('Area')
-export class AreaResolver {}
+export class AreaResolver {
+    constructor(private readonly areaServie: AreaService) {}
+
+    @Query('Areas')
+    async Areas() {
+        return await this.areaServie.listArea();
+    }
+}
