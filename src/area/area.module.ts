b6e45d89789eb77b8c0ad6063c203ea7832b15de
yee2542 <yee2542gtl@gmail.com>
2019-12-25 00:35:59 +0700

    add scalar type

--- src/area/area.module.ts
+++ src/area/area.module.ts
@@ -3,10 +3,13 @@ import { AreaService } from './area.service';
 import { AreaResolver } from './area.resolver';
 import { DatabaseModule } from '../database/database.module';
 import { areaProviders } from './area.providers';
+import { DateScalar } from 'src/common/date.scalar';
+import { JSONScalar } from 'src/common/json.scalar';
 
 @Module({
   imports: [DatabaseModule],
   providers: [
+    DateScalar, JSONScalar,
     AreaService, AreaResolver,
     ...areaProviders,
   ],
