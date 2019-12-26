a749dc3809ff3f1661e6ff8d3a537fc721e1d73b
yee2542 <yee2542gtl@gmail.com>
2019-12-26 17:16:39 +0700

    register provider & inject DB

--- src/form/form.module.ts
+++ src/form/form.module.ts
@@ -3,8 +3,11 @@ import { FormResolver } from './form.resolver';
 import { FormService } from './form.service';
 import { DateScalar } from 'src/common/date.scalar';
 import { JSONScalar } from 'src/common/json.scalar';
+import { DatabaseModule } from 'src/database/database.module';
+import { formProviders } from './form.providers';
 
 @Module({
-  providers: [DateScalar, JSONScalar, FormResolver, FormService],
+  imports: [DatabaseModule],
+  providers: [DateScalar, JSONScalar, FormResolver, FormService, ...formProviders],
 })
-export class FormModule {}
+export class FormModule { }
