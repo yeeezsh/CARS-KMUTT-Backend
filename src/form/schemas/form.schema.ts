567773d32fd11dc3f4e0c7c16f915ac8e72883b4
yee2542 <yee2542gtl@gmail.com>
2019-12-26 17:57:46 +0700

    fix general issues

--- src/form/schemas/form.schema.ts
+++ src/form/schemas/form.schema.ts
@@ -1,21 +1,25 @@
 import { Schema } from 'mongoose';
 
-const option = {
+const option = new Schema({
   key: String,
+  label: String,
   value: String,
-};
+});
 
-const field = {
+const field = new Schema({
   key: String,
+  label: String,
   value: String,
   type: String,
   options: [option],
+  // options: [],
   required: { type: Boolean, default: false },
-};
+});
 
 export const FormSchema = new Schema({
   label: String,
   fields: [field],
+  // fields: [],
   createAt: { type: Date, default: Date.now },
   updateAt: { type: Date, default: Date.now },
 });
