7ff044cb1eff906a339fd9cc4c441c6bfb23fca9
yee2542 <yee2542gtl@gmail.com>
2019-12-26 17:16:52 +0700

    initialize form providers

--- src/form/form.providers.ts
+++ src/form/form.providers.ts
@@ -1,1 +1,11 @@
 import { Connection } from 'mongoose';
+import { FormSchema } from './schemas/form.schema';
+
+export const formProviders = [
+    {
+        provide: 'FORM_MODEL',
+        useFactory: async (connection: Connection) =>
+          await connection.model('forms', FormSchema),
+        inject: ['DATABASE_CONNECTION'],
+      },
+];
