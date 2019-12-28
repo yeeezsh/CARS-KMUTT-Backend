161e575053105834039258da5ca703f51fa12bd1
yee2542 <yee2542gtl@gmail.com>
2019-12-29 00:35:48 +0700

    update schema

--- src/area/schemas/area.building.schema.ts
+++ src/area/schemas/area.building.schema.ts
@@ -3,6 +3,7 @@ import { Schema } from 'mongoose';
 export const AreaBuildingSchema = new Schema({
   name: String,
   type: { type: String, required: true, enum: ['sport', 'area'] },
+  category: String,
   createAt: { type: Date, default: Date.now },
   updateAt: { type: Date, default: Date.now },
 });
