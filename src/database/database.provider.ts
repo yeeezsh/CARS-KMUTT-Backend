d953915e187d667cd8e3815345d9db3d93dba4a1
yee2542 <yee2542gtl@gmail.com>
2020-01-21 22:22:42 +0700

    use authen

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -5,6 +5,8 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
       await mongoose.connect('mongodb://127.0.0.1:27017/cars-kmutt', {
+        user: 'root',
+        pass: 'kmuttC@Rs2020',
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
