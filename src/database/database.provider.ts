a2555e8355025a897a24a38841ac61449e687a7e
yee2542 <yee2542gtl@gmail.com>
2020-01-21 22:27:56 +0700

    fix authen issues

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -4,9 +4,10 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://127.0.0.1:27017/cars-kmutt', {
+      await mongoose.connect('mongodb://mongodb-sharded:27017/cars-kmutt', {
         user: 'root',
         pass: 'kmuttC@Rs2020',
+        authSource: 'admin',
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
