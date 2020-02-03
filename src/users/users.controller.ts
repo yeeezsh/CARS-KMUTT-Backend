df52323bbabadb4fdd9782ddc3216e427a58a867
yee2542 <yee2542gtl@gmail.com>
2020-02-04 00:09:39 +0700

    set cookie instead sending key (deprecated next patch)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,23 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly authService: AuthService) {}
   @Post('/auth/staff')
-  async loginStaff(@Body() body) {
+  async loginStaff(@Body() body, @Res() res: Response) {
     const { username, password } = body;
-    return await this.authService.loginStaff(username, password);
+    const user = await this.authService.loginStaff(username, password);
+    res.cookie('Authorization', user.Authorization);
+    return res.send(user);
   }
 
   @Post('/auth/requestor')
-  async loginRequestor(@Body() body) {
+  async loginRequestor(@Body() body, @Res() res: Response) {
     const { username, password } = body;
-    return await this.authService.loginRequestor(username, password);
+    const user = await this.authService.loginRequestor(username, password);
+    res.cookie('Authorization', user.Authorization);
+    return res.send(user);
   }
 }
