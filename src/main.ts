b3bf089c7db4602d6f9f25b3a58b9c03679479ae
yee2542 <yee2542gtl@gmail.com>
2020-02-04 00:45:56 +0700

    fix cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
   app.use(cookieParser());
