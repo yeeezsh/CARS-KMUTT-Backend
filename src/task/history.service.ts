39a21be7bd18b86f1c39fa431cde53bbea813a51
yee2542 <yee2542gtl@gmail.com>
2020-02-10 22:23:10 +0700

    clean up code

--- src/task/history.service.ts
+++ src/task/history.service.ts
@@ -2,10 +2,13 @@ import { Model } from 'mongoose';
 import { Injectable, Inject } from '@nestjs/common';
 import { Task } from './interfaces/task.interface';
 
+const DEFAULT_SELECT = 'state cancle reserve requestor area createAt';
+const DEAFAULT_POPULATE = 'area';
+const DEAFAULT_POPULATE_SELECT = '_id name label building';
+
 @Injectable()
 export class HistoryService {
   constructor(@Inject('TASK_MODEL') private readonly taskModel: Model<Task>) {}
-
   async getAllHistory(username: string): Promise<Task[]> {
     return await this.taskModel
       .find({
@@ -15,8 +18,8 @@ export class HistoryService {
           },
         },
       })
-      .select('state cancle reserve requestor area createAt')
-      .populate('area', '_id name label building')
+      .select(DEFAULT_SELECT)
+      .populate(DEAFAULT_POPULATE, DEAFAULT_POPULATE_SELECT)
       .sort({ createAt: -1 });
   }
 }
