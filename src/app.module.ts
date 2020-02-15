a62288a402482a5785d3a1064374898a5921a622
yee2542 <yee2542gtl@gmail.com>
2020-02-15 15:09:21 +0700

    add crons dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
