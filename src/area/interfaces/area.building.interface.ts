a9fd4cd5837d18de3db30e5bbb1fe068da3de72c
yee2542 <yee2542gtl@gmail.com>
2020-03-13 18:16:33 +0700

    Refactor

--- src/area/interfaces/area.building.interface.ts
+++ src/area/interfaces/area.building.interface.ts
@@ -1,9 +1,10 @@
 import { Document } from 'mongoose';
+import { AreaBuildingType } from './building.type';
 
 export interface AreaBuilding extends Document {
   name: string;
   label?: string;
-  type: 'sport' | 'area' | 'meeting';
+  type: AreaBuildingType;
   createAt: Date;
   updateAt: Date;
 }
---
+++ src/area/interfaces/building.type.ts
@@ -0,0 +1,1 @@
+export type AreaBuildingType = 'sport' | 'area' | 'meeting';
