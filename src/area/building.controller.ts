a01c52d6882abbfe72aa5387312521a9ee90a733
yee2542 <yee2542gtl@gmail.com>
2020-04-20 01:16:49 +0700

    Add query by  building id

--- src/area/building.controller.ts
+++ src/area/building.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Post, UseGuards, Get, Body, Param } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AreaService } from './area.service';
-import { CreateAreaBuildingDto } from './dtos/area.building.create.dto';
-import { CreateAreaDto } from './dtos/area.create.dto';
-import moment = require('moment');
+import { Controller, Get, Param } from '@nestjs/common';
+import { AreaQueryService } from './area.query.service';
 
 @Controller('building')
 export class BuildingController {
+  constructor(private readonly areaQueryService: AreaQueryService) {}
+
   @Get('/table')
   async getBuildingTable() {
     return;
+  }
+
+  // query building only
+  @Get('/:id')
+  async getBuilding(@Param('id') id: string) {
+    return (await this.areaQueryService.getAreaBuilding(id))[0];
   }
 }
