724dab87d2793e05897226fe9017dc1297f6413e
yee2542 <yee2542gtl@gmail.com>
2020-04-21 01:36:36 +0700

    Map area and building

--- src/task/history.service.ts
+++ src/task/history.service.ts
@@ -3,14 +3,20 @@ import { Injectable, Inject } from '@nestjs/common';
 import { Task } from './interfaces/task.interface';
 
 const DEFAULT_SELECT = '_id state cancle reserve requestor area createAt';
-const DEAFAULT_POPULATE = 'area';
+const DEAFAULT_POPULATE = 'area building';
 const DEAFAULT_POPULATE_SELECT = '_id name label building';
+
+function mapAreaBuilding(e) {
+  return e.building ? { ...e, area: e.building } : e;
+}
 
 @Injectable()
 export class HistoryService {
-  constructor(@Inject('TASK_MODEL') private readonly taskModel: Model<Task>) {}
+  constructor(
+    @Inject('TASK_MODEL') private readonly taskModel: Model<Task | any>,
+  ) {}
   async getAllHistory(username: string): Promise<Task[]> {
-    return await this.taskModel
+    const doc = await this.taskModel
       .find({
         requestor: {
           $elemMatch: {
@@ -20,11 +26,14 @@ export class HistoryService {
       })
       .select(DEFAULT_SELECT)
       .populate(DEAFAULT_POPULATE, DEAFAULT_POPULATE_SELECT)
-      .sort({ createAt: -1 });
+      .sort({ createAt: -1 })
+      .lean();
+    const parsed = doc.map(mapAreaBuilding);
+    return parsed;
   }
 
   async getAllRequested(username: string): Promise<Task[]> {
-    return await this.taskModel
+    const doc = await this.taskModel
       .find({
         requestor: {
           $elemMatch: {
@@ -38,11 +47,15 @@ export class HistoryService {
       })
       .select(DEFAULT_SELECT)
       .populate(DEAFAULT_POPULATE, DEAFAULT_POPULATE_SELECT)
-      .sort({ createAt: -1 });
+      .sort({ createAt: -1 })
+      .lean();
+
+    const parsed = doc.map(mapAreaBuilding);
+    return parsed;
   }
 
   async getAllWait(username: string): Promise<Task[]> {
-    return await this.taskModel
+    const doc = await this.taskModel
       .find({
         requestor: {
           $elemMatch: {
@@ -56,6 +69,10 @@ export class HistoryService {
       })
       .select(DEFAULT_SELECT)
       .populate(DEAFAULT_POPULATE, DEAFAULT_POPULATE_SELECT)
-      .sort({ createAt: -1 });
+      .sort({ createAt: -1 })
+      .lean();
+
+    const parsed = doc.map(mapAreaBuilding);
+    return parsed;
   }
 }
