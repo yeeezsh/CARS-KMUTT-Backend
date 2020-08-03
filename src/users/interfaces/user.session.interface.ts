db7a114b3d725430e280112b0fbd116d725b1a73
yee2542 <yee2542gtl@gmail.com>
2020-08-03 22:33:35 +0700

    Add group type for staff types

--- src/users/interfaces/user.session.interface.ts
+++ src/users/interfaces/user.session.interface.ts
@@ -1,7 +1,10 @@
+import { StaffPermissionType } from '../schemas/staffs.schema';
+
 export interface UserSession {
   _id: string;
   username: string;
   studentId: string;
   email: string;
   permission: string;
+  group: string | StaffPermissionType;
 }
