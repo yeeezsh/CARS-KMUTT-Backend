de57683989fffec142b07421aa6c3bc4f8af098d
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-12 12:41:51 +0700

    Add Config interface

---
+++ src/config/configuration.interface.ts
@@ -0,0 +1,14 @@
+export interface ConfigurationInterface {
+  database: {
+    connection: string;
+    username: string;
+    password: string;
+    authSource: string;
+  };
+  jwt: {
+    secretKey: string;
+    expires: string;
+  };
+  opsKey: string;
+  node_env: 'production' | 'development';
+}
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,10 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+import { ConfigurationInterface } from './configuration.interface';
+
+export default (): ConfigurationInterface => ({
   database: {
     connection:
       process.env.DATABASE_HOST || 'mongodb://mongodb-sharded:27017/cars-kmutt',
-    username: process.env || 'root',
+    username: process.env.DATABASE_USERAME || 'root',
     password: '' || 'kmuttC@Rs2020',
     authSource: '' || 'admin',
   },
@@ -11,4 +12,8 @@ export default () => ({
     secretKey: '' || 'secretKeyJwtEiei',
     expires: '' || '3600s',
   },
+  opsKey: '' || 'kmuttC@Rs2020.OpsKey',
+  node_env:
+    (process.env.NODE_ENV as ConfigurationInterface['node_env']) ||
+    'development',
 });
