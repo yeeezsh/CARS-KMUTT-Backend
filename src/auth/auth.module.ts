c847c4c5cafa1d94db881ccd80bf1de947970d55
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-12 15:41:09 +0700

    Deprecated JWT constant

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 import { RequestorJWTStrategy } from './requestor.jwt.strategy';
 import { StaffJWTStrategy } from './staff.jwt.strategy';
 
