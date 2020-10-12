14fcc2ab73b0de04f162f1faffbd838ab381dd97
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-12 15:35:03 +0700

    import config module

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,17 +1,28 @@
+import { Provider } from '@nestjs/common';
 import * as mongoose from 'mongoose';
+import { ConfigurationInterface } from 'src/config/configuration.interface';
+// import { ConfigModule } from '../config/config.module';
 
-export const databaseProviders = [
+export const databaseProviders: Provider[] = [
   {
+    inject: ['APP_CONFIG'],
     provide: 'DATABASE_CONNECTION',
-    useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://mongodb-sharded:27017/cars-kmutt', {
-        user: 'root',
-        pass: 'kmuttC@Rs2020',
-        authSource: 'admin',
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-      }),
+    useFactory: async (
+      appConfig: ConfigurationInterface,
+    ): Promise<typeof mongoose> => {
+      console.log(appConfig.database.authSource);
+      return await mongoose.connect(
+        'mongodb://mongodb-sharded:27017/cars-kmutt',
+        {
+          user: 'root',
+          pass: 'kmuttC@Rs2020',
+          authSource: 'admin',
+          useCreateIndex: true,
+          useNewUrlParser: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true,
+        },
+      );
+    },
   },
 ];
