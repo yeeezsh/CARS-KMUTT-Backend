732262549090adbc7720f5c9dced32e5eaf44d24
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-12 12:49:50 +0700

    inject config from process

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,19 +1,31 @@
+import { ConfigType } from './config.type';
 import { ConfigurationInterface } from './configuration.interface';
 
-export default (): ConfigurationInterface => ({
-  database: {
-    connection:
-      process.env.DATABASE_HOST || 'mongodb://mongodb-sharded:27017/cars-kmutt',
-    username: process.env.DATABASE_USERAME || 'root',
-    password: '' || 'kmuttC@Rs2020',
-    authSource: '' || 'admin',
-  },
-  jwt: {
-    secretKey: '' || 'secretKeyJwtEiei',
-    expires: '' || '3600s',
-  },
-  opsKey: '' || 'kmuttC@Rs2020.OpsKey',
-  node_env:
-    (process.env.NODE_ENV as ConfigurationInterface['node_env']) ||
-    'development',
-});
+export default (): ConfigurationInterface => {
+  const {
+    DATABASE_CONNECTION,
+    DATABASE_USERAME,
+    DATABASE_PASSWORD,
+    DATABASE_AUTH_SOURCE,
+    JWT_SECRET_KEY,
+    JWT_EXPIRES,
+    OPS_KEY,
+  } = process.env as ConfigType;
+  return {
+    database: {
+      connection:
+        DATABASE_CONNECTION || 'mongodb://mongodb-sharded:27017/cars-kmutt',
+      username: DATABASE_USERAME || 'root',
+      password: DATABASE_PASSWORD || 'kmuttC@Rs2020',
+      authSource: DATABASE_AUTH_SOURCE || 'admin',
+    },
+    jwt: {
+      secretKey: JWT_SECRET_KEY || 'secretKeyJwtEiei',
+      expires: JWT_EXPIRES || '3600s',
+    },
+    opsKey: OPS_KEY || 'kmuttC@Rs2020.OpsKey',
+    node_env:
+      (process.env.NODE_ENV as ConfigurationInterface['node_env']) ||
+      'development',
+  };
+};
